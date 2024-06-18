031aa55dabe7293eb793ecbe6064bfa693126ec7
yoojiwon <loveujjiwon@gmail.com>
2024-06-19 01:43:53 +0900

    ✨update 타워 추가 구입, 업그레이드, 환불 기능 구현/ 테스트 완료

--- src/handlers/handlerMapping.js
+++ src/handlers/handlerMapping.js
@@ -1,10 +1,14 @@
-import { gameStart, gameEnd } from './game.handler.js';
+import { gameStart, gameEnd, handleTowerEvent } from './game.handler.js';
 import { addMonsterKillCount } from './monster.handler.js';
 
 const handlerMapping = {
   2: gameStart,
   3: gameEnd,
   41: addMonsterKillCount,
+  42: handleTowerEvent,
+  43: handleTowerEvent,
+  44: handleTowerEvent,
+  45: handleTowerEvent,
 };
 
 export default handlerMapping;
