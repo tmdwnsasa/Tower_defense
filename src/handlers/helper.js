7a670cf1f6ba8de0606e1022b7fb8ee184ff94f0
ydh1503 <ydh1503@naver.com>
2024-06-19 12:24:16 +0900

    bugfix - helper.js, Socket.js
    오타 수정

--- src/handlers/helper.js
+++ src/handlers/helper.js
@@ -21,7 +21,7 @@ export const handlerEvent = (io, socket, data) => {
     return;
   }
 
-  const response = handler(data.id, data.payload);
+  const response = handler(data.userId, data.payload);
 
   if (response.broadcast) {
     io.emit('response', 'response');
